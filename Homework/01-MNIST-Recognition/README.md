638ee157d707f6a9c96a92dbf62155a20b055302
Arthals <zhuozhiyongde@126.com>
2024-03-11 21:09:42 +0800

    feat: np_mnist

--- Homework/01-MNIST-Recognition/README.md
+++ Homework/01-MNIST-Recognition/README.md
@@ -1,21 +1,11 @@
-# Homework 1 MNIST Recognition
+# MNIST-Recognition
 
-## 作业 1：用 numpy 实现训练 MLP 网络识别手写数字 MNIST 数据集
+Softmax 导数证明
 
-运行、阅读并理解反向传播算法示例 bp_np.py
-
-修改 np_mnist_template.py，更改 loss 函数、网络结构、激活函数，完成训练 MLP 网络识别手写数字 MNIST 数据集。
-
-要求：10 个 epoch 后测试集准确率达到 94%以上
-
-提交方式：np_mnist.py 代码文件，训练过程中打印的准确率截图
-
-## 作业 2：使用 Pytorch 训练 MNIST 数据集的 MLP 模型
-
-运行、阅读并理解 mnist_mlp_template.py,修改网络结构和参数，增加隐藏层，观察训练效果
-
-使用 Adam 等不同优化器，添加 Dropout 层，观察训练效果
-
-要求：10 个 epoch 后测试集准确率达到 97%以上
-
-提交方式：mnist_mlp.py 代码文件，训练过程中打印的准确率截图
+$$
+\begin{align*}
+\frac{\partial S(\mathbf{x})_j}{\partial x_i} &= \frac{\partial}{\partial x_i} \left(\frac{e^{x_j}}{\sum_{k=1}^n e^{x_k}}\right) \\
+&= \frac{e^{x_j}\delta_{ij}\sum_{k=1}^n e^{x_k} - e^{x_j}e^{x_i}}{\left(\sum_{k=1}^n e^{x_k}\right)^2} \\
+&= \delta_{ij} S(\mathbf{x})_j - S(\mathbf{x})_i S(\mathbf{x})_j
+\end{align*}
+$$
--- Homework/01-MNIST-Recognition/np_mnist_template.py
+++ Homework/01-MNIST-Recognition/np_mnist_template.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-@ author: Yiliang Liu
-"""
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Author  :   Arthals
+# @File    :   np_mnist_template.py
+# @Time    :   2024/03/10 04:26:45
+# @Contact :   zhuozhiyongde@126.com
+# @Software:   Visual Studio Code
 
 
 # 作业内容：更改loss函数、网络结构、激活函数，完成训练MLP网络识别手写数字MNIST数据集
@@ -30,29 +33,31 @@ def relu(x):
     """
     relu函数
     """
-    pass
+    return np.maximum(0, x)
 
 
 def relu_prime(x):
     """
     relu函数的导数
     """
-    pass
+    return np.where(x > 0, 1, 0)
 
 
 # 输出层激活函数
-def f(x):
+def softmax(x: np.ndarray) -> np.ndarray:
     """
     softmax函数, 防止除0
+    x: (batch_size, dimensions)
     """
-    pass
+    stable_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return stable_x / np.sum(stable_x, axis=1, keepdims=True)
 
 
-def f_prime(x):
+def softmax_prime(x):
     """
     softmax函数的导数
     """
-    pass
+    return softmax(x) * (1.0 - softmax(x))
 
 
 # 定义损失函数
@@ -61,7 +66,7 @@ def loss_fn(y_true, y_pred):
     y_true: (batch_size, num_classes), one-hot编码
     y_pred: (batch_size, num_classes), softmax输出
     """
-    pass
+    return -np.sum(y_true * np.log(y_pred + 1e-8), axis=-1)
 
 
 def loss_fn_prime(y_true, y_pred):
@@ -69,7 +74,7 @@ def loss_fn_prime(y_true, y_pred):
     y_true: (batch_size, num_classes), one-hot编码
     y_pred: (batch_size, num_classes), softmax输出
     """
-    pass
+    return y_pred - y_true
 
 
 # 定义权重初始化函数
@@ -90,29 +95,55 @@ class Network(object):
         """
         初始化网络结构
         """
-        pass
+        self.lr = lr
+        self.W1 = init_weights((input_size + 1, hidden_size))
+        self.W2 = init_weights((hidden_size, hidden_size))
+        self.W3 = init_weights((hidden_size, output_size))
 
     def forward(self, x):
         """
         前向传播
         """
-        pass
+        # 将 x 添加偏置
+        x = np.hstack([x, np.ones((x.shape[0], 1))])
+        self.z1 = x @ self.W1
+        self.a1 = relu(self.z1)
+        self.z2 = self.a1 @ self.W2
+        self.a2 = relu(self.z2)
+        self.z3 = self.a2 @ self.W3
+        self.a3 = softmax(self.z3)
+        return self.a3
 
     def step(self, x_batch, y_batch):
         """
         一步训练
         """
+        batch_size = x_batch.shape[0]
 
         # 前向传播
-        pass
+        y_pred = self.forward(x_batch)
+
         # 计算损失和准确率
-        pass
+        loss = np.mean(loss_fn(y_batch, y_pred))
+        acc = np.mean(np.argmax(y_pred, axis=1) == np.argmax(y_batch, axis=1))
 
         # 反向传播
-        pass
+        grad_a3 = loss_fn_prime(y_batch, y_pred)
+        grad_W3 = self.a2.T @ grad_a3 / batch_size
+
+        grad_a2 = grad_a3 @ self.W3.T * relu_prime(self.z2)
+        grad_W2 = self.a1.T @ grad_a2 / batch_size
+
+        grad_a1 = grad_a2 @ self.W2.T * relu_prime(self.z1)
+        x_prime = np.hstack([x_batch, np.ones((x_batch.shape[0], 1))])
+        grad_W1 = x_prime.T @ grad_a1 / batch_size
 
         # 更新权重
-        pass
+        self.W3 -= self.lr * grad_W3
+        self.W2 -= self.lr * grad_W2
+        self.W1 -= self.lr * grad_W1
+
+        return loss, acc
 
 
 if __name__ == "__main__":
@@ -123,4 +154,16 @@ if __name__ == "__main__":
         accuracies = []
         p_bar = tqdm(range(0, len(X_train), 64))
         for i in p_bar:
-            pass
+            x_batch = X_train[i : i + 64]
+            y_batch = y_train[i : i + 64]
+            loss, acc = net.step(x_batch, y_batch)
+            losses.append(loss)
+            accuracies.append(acc)
+
+            p_bar.set_description(f"loss: {loss:.4f}, acc: {acc:.4f}")
+
+        # 验证网络
+        y_pred = net.forward(X_val)
+        val_loss = np.mean(loss_fn(y_val, y_pred))
+        val_acc = np.mean(np.argmax(y_pred, axis=-1) == np.argmax(y_val, axis=-1))
+        print(f"epoch: {epoch}, val_loss: {val_loss:.4f}, val_acc: {val_acc:.4f}")
